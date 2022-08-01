44a740a60fa1cb8a8a0b067e217982c1af01b8d0
JlordS32 <jlordsty128@gmail.com>
2022-08-01 22:36:56 +1000

    ok

--- Sol_ConsoleApp1/ConsoleApp1/Program.cs
+++ Sol_ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //string personName = "George";
+            //int personAge;
+            //personAge = 71;
+
+            //Console.WriteLine($"There once was a man named {personName}");
+            //Console.WriteLine($"He was {personAge} years old");
+            //personName = "Mike";
+            //Console.WriteLine($"He really liked the name {personName}");
+            //Console.WriteLine(String.Format("But didn't liked being {0}.", personAge));
+
+            //Console.ReadLine();
+
+            string daBebe = "bebe";
+            string phrase = $"Meme is the best {daBebe}.";
+            bool ifTrue = phrase.Contains("bebe");
+            if(ifTrue is true)
+            {
+                Console.WriteLine("There's a bebe.");
+            }
+            else
+            {
+                Console.WriteLine("There's no bebe.");
+            }
+            Console.WriteLine(phrase.IndexOf('z'));
+            Console.ReadLine();
         }
     }
 }
