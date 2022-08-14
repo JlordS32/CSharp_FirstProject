5626241ab6a5bcfdd20ac3e76b6b882756e11a7e
JlordS32 <jlordsty128@gmail.com>
2022-08-14 20:26:33 +1000

    new commit

---
+++ Sol_testResults/regexTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace regexTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"\D");
+            Match match = regex.Match("Number 3434");
+
+            if (match.Success)
+            {
+                Console.WriteLine(match.Value);
+            }
+
+            Console.ReadLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
