a1c24588a08375593397b00d69eb4678e399132d
JlordS32 <jlordsty128@gmail.com>
2022-09-07 21:22:06 +1000

    new changes

---
+++ Sol_classAbstract/classAbstract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classAbstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Name = "Circle";
+        }
+        public override double Area()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sol_classAbstract/classAbstract/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classAbstract
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("This is a {0}", Name);
+        }
+
+        public abstract double Area();
+    }
+}
