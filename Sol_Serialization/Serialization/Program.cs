34b62cc3249cabe3923e2275a77893d094e2a134
JlordS32 <jlordsty128@gmail.com>
2022-09-24 23:47:31 +1000

    #

---
+++ Sol_Serialization/Serialization/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal bowser = new Animal("Bowser", 45, 25);
+
+            Stream stream = File.Open("Animal.dat", FileMode.Create);
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(stream, bowser);
+
+            Console.ReadLine();
+        }
+    }
+}
