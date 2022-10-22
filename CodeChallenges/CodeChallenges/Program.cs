66777eed5f538ba3e92c6eaf970ff0906208c2ef
JlordS32 <jlordsty128@gmail.com>
2022-10-22 18:53:43 +1100

    #

--- CodeChallenges/CodeChallenges/Program.cs
+++ CodeChallenges/CodeChallenges/Program.cs
@@ -8,8 +8,24 @@ namespace CodeChallenges
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int[] num = { 2, 9, 11, 7 };
+            int output = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                output = num[i] + num[i+1];
+
+                if (output == 9)
+                {
+                    Console.WriteLine("[{0}, {1}]", Array.IndexOf(num, num[i]), Array.IndexOf(num, num[i+1]));
+                    break;
+                }
+                else Console.WriteLine("Nothing");
+            }
+
+            Console.ReadLine();
         }
     }
 }
