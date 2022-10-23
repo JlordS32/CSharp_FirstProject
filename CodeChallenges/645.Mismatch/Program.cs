ae0ed19cb82eb6133bdfa7c9dbfcd074a67dd5b4
JlordS32 <jlordsty128@gmail.com>
2022-10-24 00:44:03 +1100

    new commit

---
+++ CodeChallenges/645.Mismatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _645.Mismatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] s = { 1, 2, 2, 4 };
+            int[] repeatedNumber = new int[s.Length];
+            int[] correctNumber = new int[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i == 0) continue;
+                else if (s[i] == s[i - 1])
+                {
+                    repeatedNumber[0] = s[i];
+                    correctNumber[0] = s[i] + 1;
+                }
+            }
+
+            Console.WriteLine("[{0}, {1}]", repeatedNumber[0], correctNumber[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
--- CodeChallenges/CodeChallenges/Program.cs
+++ CodeChallenges/CodeChallenges/Program.cs
@@ -10,16 +10,36 @@ namespace CodeChallenges
     {
         static void Main()
         {
+            string s = "foo";
+            string[] aList = { "foo", "bar", "baaz" };
+
+
+            if (aList.Contains(s))
+            {
+                Console.WriteLine($"{s} exists");
+            }
+            else Console.WriteLine("Item does not exist");
+
+            Console.WriteLine(Math.Pow(10, 4));
+
+            int[] array1 = new int[4];
+            array1[0] = 1;
+
+            Console.ReadLine();
+        }
+
+        static void PracticeOne()
+        {
             int[] num = { 2, 9, 11, 7 };
             int output = 0;
 
             for (int i = 0; i < num.Length; i++)
             {
-                output = num[i] + num[i+1];
+                output = num[i] + num[i + 1];
 
                 if (output == 9)
                 {
-                    Console.WriteLine("[{0}, {1}]", Array.IndexOf(num, num[i]), Array.IndexOf(num, num[i+1]));
+                    Console.WriteLine("[{0}, {1}]", Array.IndexOf(num, num[i]), Array.IndexOf(num, num[i + 1]));
                     break;
                 }
                 else Console.WriteLine("Nothing");
