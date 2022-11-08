83617966fa96cdbf2d6d0b5659ab4cfdd4804f51
JlordS32 <jlordsty128@gmail.com>
2022-11-08 23:57:53 +1100

    #

---
+++ CodeChallenges/21.MergeTwoLists/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.MergeTwoLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ListNode list1 = new ListNode(1, new ListNode(2));
+            ListNode list2 = new ListNode(3, new ListNode(4));
+            var solution = new Solution();
+
+            Console.WriteLine(solution.MergeTwoList(list1, list2));
+
+            Console.ReadLine();
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public class Solution
+        {
+            public ListNode MergeTwoList(ListNode list1, ListNode list2)
+            {
+                return list1;
+            }
+        }
+    }
+}
--- Sol_Recursion/Recursion_2/Program.cs
+++ Sol_Recursion/Recursion_2/Program.cs
@@ -10,6 +10,23 @@ namespace Recursion_2
     {
         static void Main(string[] args)
         {
+            string word = "nitin";
+            var parts = new List<string>();
+
+            FindPalindrome(word.Length, parts, word);
         }
+
+        static void FindPalindrome(int n, List<string> parts, string s)
+        {
+            if (n == 0) return;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                
+            }
+
+            FindPalindrome(n, parts, s);
+        }
+
     }
 }
